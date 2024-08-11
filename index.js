5996bef7ef63dc5a35f09dd5c00a5e7cbe2e538d
Rachell Aristo <123459774+sleepycodingcat@users.noreply.github.com>
2024-08-12 07:48:33 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+import {
+  Project,
+  Sprite,
+} from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+
+import Stage from "./Stage/Stage.js";
+import Cookie from "./Cookie/Cookie.js";
+
+const stage = new Stage({ costumeNumber: 1 });
+
+const sprites = {
+  Cookie: new Cookie({
+    x: 0,
+    y: 0,
+    direction: 90,
+    rotationStyle: Sprite.RotationStyle.ALL_AROUND,
+    costumeNumber: 1,
+    size: 350,
+    visible: true,
+    layerOrder: 1,
+  }),
+};
+
+const project = new Project(stage, sprites, {
+  frameRate: 30, // Set to 60 to make your project run faster
+});
+export default project;
